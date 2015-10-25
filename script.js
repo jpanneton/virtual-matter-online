9c39612be47555d28b69020dc9e4a196cf1de78b
Jérémi Panneton <jeremi.panneton@outlook.com>
2015-10-25 00:20:07 -0400

    Website
    Virtual Matter - Website

---
+++ script.js
@@ -0,0 +1,66 @@
+function generateRandomBoxShadow(animated) {
+    var content = document.getElementById("content");
+    content.className = animated ? "animated-box-shadow" : "fixed-box-shadow";
+    
+    var randomColor = '#' + Math.random().toString(16).slice(2, 8);
+
+    if(animated) {
+        $("#content").css('box-shadow', 'none');
+        setTimeout(function() {
+            $("#content").css('box-shadow', '10px 10px 0px ' + randomColor);
+        }, 400);
+    }
+    else {
+        $("#content").css('box-shadow', '10px 10px 0px ' + randomColor);
+    }
+}
+
+$(document).ready(function () {
+    
+    // Animated box shadow
+    generateRandomBoxShadow(true);
+
+    // Home page setup
+    $("#content div").hide();
+    $("#home").show();
+    
+    // Mobile devices menu trigger
+    $("#menu-trigger").click(function() {
+        $("#nav-menu-items").slideToggle(500, function() {
+            $("#nav-menu-items").toggleClass('nav-expanded').css('display', '');
+        });
+    });
+    
+    // Navigation menu events
+    $("#nav-menu-items a").click(function() {
+        // Select menu item
+        $("#nav-menu-items a").removeClass('selected');
+        $(this).addClass('selected');
+        
+        // Last page height
+        var currentHeight = $("#content").height();
+        $('#content').css('height', 'auto');
+        
+        // Display according page
+        var page = $(this).attr('href');
+        $("#content div").filter(function(){ return ! $(this).is(":hidden"); })
+                         .fadeOut('fast', function() {
+            // New page content fade-in
+            $(page).fadeIn('slow');
+            
+            // Animated page content wrapping (animated page height)
+            var autoHeight = $("#content").height();
+            $("#content").height(currentHeight).animate( {
+                height: autoHeight
+            }, 'fast');
+        });
+        
+        // Fixed box shadow
+        generateRandomBoxShadow(false);
+    });
+});
+
+$(window).load(function () {
+
+    $(window).resize();
+});
